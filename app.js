0c23f47bb2077c52c10810d1589a5f7e57f017e9
Camara Vamoro <cvamorocamson@gmail.com>
2023-09-12 03:37:51 +0100

    firstCommit

---
+++ app.js
@@ -0,0 +1,26 @@
+let whiteDod1 = document.querySelector('.whiteDod1') 
+let whiteDod2 = document.querySelector('.whiteDod2') 
+let whiteDod11 = document.querySelector('.whiteDod11') 
+let whiteDod22 = document.querySelector('.whiteDod22') 
+let whiteDod111 = document.querySelector('.whiteDod111') 
+let whiteDod222 = document.querySelector('.whiteDod222') 
+let whiteDod1111 = document.querySelector('.whiteDod1111') 
+let whiteDod2222 = document.querySelector('.whiteDod2222') 
+let whiteDod11111 = document.querySelector('.whiteDod11111') 
+let whiteDod22222 = document.querySelector('.whiteDod22222') 
+let whiteDod111111 = document.querySelector('.whiteDod111111') 
+let whiteDod222222 = document.querySelector('.whiteDod222222') 
+
+
+let number1 = Math.floor(Math.random()*6 + 1)
+let number2 = Math.floor(Math.random()*6 + 1)
+
+
+
+
+function random(number){
+    if(number === 1){
+        
+    }
+}
+
